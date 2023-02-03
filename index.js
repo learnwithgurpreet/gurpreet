88bd23125964ef602774e015154624ecbd22f6b9
Gurpreet Singh <gursingh7@publicisgroupe.net>
2023-02-03 10:54:03 +0100

    added colors

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 console.log(`
+
 Hi I'm Gurpreet
 
 I like creating and using specialized tools for making development processes as efficient as possible. I have the experience to work with big projects and big development teams.
 
 Currently working as senior front-end architect at PublicisSapient.
-
-📖 Blog: https://learnwithgurpreet.com
-👾 GitHub: https://github.com/learnwithgurpreet
-
 `);
+
+console.log(
+  "📖 Blog:   ",
+  "\x1b[36m",
+  "https://learnwithgurpreet.com",
+  "\x1b[0m"
+);
+console.log(
+  "👾 GitHub: ",
+  "\x1b[36m",
+  "https://github.com/learnwithgurpreet",
+  "\x1b[0m"
+);
+console.log("\n");
