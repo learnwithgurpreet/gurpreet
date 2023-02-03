54de0c145e82c1c20ccb4707cf28c02494ab13db
Gurpreet Singh <gursingh7@publicisgroupe.net>
2023-02-03 10:29:36 +0100

    changed introduction

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 console.log(`
-Hello World!
+Hi I'm Gurpreet
 
-I'm Gurpreet Singh, a Senior Architect in Frontend Technology working with PublicisSapient. Having more than 12 years of experience in the area of JavaScript, HTML5, and CSS3. I like creating and using specialized tools for making development processes as efficient as possible. I have the experience to work with big projects and big development teams.
+I like creating and using specialized tools for making development processes as efficient as possible. I have the experience to work with big projects and big development teams.
+
+Currently working as senior front-end architect at PublicisSapient.
 
 📖 Blog: https://learnwithgurpreet.com
 👾 GitHub: https://github.com/learnwithgurpreet
